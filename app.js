c5fc795513585b0722703a5e90bb8cc8d0599c5a
Lorenzo <lorenzolsbit@gmail.com>
2022-05-16 11:07:49 +0200

    Listo para empezar

--- app.js
+++ app.js
@@ -7,19 +7,11 @@ var indexRouter = require('./routes/index');
 var poolRouter = require('./routes/pool');
 var authRouter = require('./routes/auth');
 
-
 require('dotenv').config()
 let models = require('./models')
-let passport = require('passport')
-let bodyParser = require('body-parser')
-let session = require('express-session')
+
 
 var app = express();
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,8 +23,6 @@ app.use('/', indexRouter);
 app.use('/pool', poolRouter);
 app.use('/auth', authRouter);
 
-//Models
-require('./config/passport/passport.js')(passport, models.user);
 
 //Sync Database
 models.sequelize.sync().then(function() { 
@@ -40,14 +30,5 @@ models.sequelize.sync().then(function() {
 }).catch(function(err) {
     console.log(err, "BD Error") 
 });
-
-//passport
-app.use(session({
-    secret: 'cosamacosapi',
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-let passport = require('passport')
-let User = models.user
 
 router.get('/signup', function(req, res, next) {
   res.send('Estas en signup');
@@ -13,22 +10,25 @@ router.get('/signup', function(req, res, next) {
 //     failureRedirect: '/signup'
 // }));
 
-router.post('/signup', passport.authenticate('local-signup'),
-  function(req, res) {
-    res.status(200).send({
-      session: 'true', user: req.user.id
-    })
-  },
-  function(req, res) {
-    res.status(404).send({
-      id: 'usuarioNo'
-    })
-  }
-);
+// router.post('/signup', passport.authenticate('local-signup'),
+//   function(req, res) {
+//     res.status(200).send({
+//       session: 'true', user: req.user.id
+//     })
+//   },
+//   function(req, res) {
+//     res.status(404).send({
+//       id: 'usuarioNo'
+//     })
+//   }
+// );
+
+router.post('/signup', function(req, res, next) {
+
+});
 
 router.post('/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
+
 });
 
 module.exports = router;
