b3257094b86ab62a070e7783bb13fda1bb94239e
Lorenzo <lorenzolsbit@gmail.com>
2022-05-25 00:52:35 +0200

    Modificado nodemail para usar la api de google

--- routes/reset.js
+++ routes/reset.js
@@ -3,15 +3,24 @@ var router = express.Router();
 const User = require("../models").User;
 const jwt = require("jsonwebtoken");
 const nodemailer = require('nodemailer');
-let smtpTransport = require('nodemailer-smtp-transport')
+// let smtpTransport = require('nodemailer-smtp-transport')
 let bcrypt = require('bcryptjs');
 const { is } = require('express/lib/request');
 require('dotenv').config()
+const { google } = require('googleapis')
 
 router.post('/email', async function(req, res, next) {
     console.log("email: ",req.body.email)
 
     try {
+        const oAuth2Client = new google.auth.OAuth2(
+            process.env.CLIETN_ID,
+            process.env.CLIENT_SECRET,
+            process.env.REDIRECT_URI
+        )
+
+        oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN})
+
         const exsitUser = await User.findOne({where: { email: req.body.email }})
 
         if(exsitUser == null){
@@ -27,15 +36,21 @@ router.post('/email', async function(req, res, next) {
             tokenresetpassword: token
         })
         
+        const accessToken = await oAuth2Client.getAccessToken()
 
-        const transporter = nodemailer.createTransport(smtpTransport({
-            host: 'smtp.gmail.com',
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            port: 465,
+            secure: true,
             auth: {
-                type: "login",
+                type: "OAuth2",
                 user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
+                clientId: process.env.CLIETN_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN,
+                accessToken: accessToken
             }
-        }))
+        })
 
         transporter.verify(function (error, success) {
             if (error) {
